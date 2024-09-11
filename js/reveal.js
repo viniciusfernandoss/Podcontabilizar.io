0f96d483c0b21d3272218c16c05e88dc5411535a
viniciusfernandoss <vinicius.senac.ads@gmail.com>
2024-09-11 01:15:55 -0300

    casa 11/09/2024

---
+++ js/reveal.js
@@ -0,0 +1,79 @@
+/*
+========================================
+            ScrollReveal
+========================================
+*/
+window.revelar = ScrollReveal({reset:true})
+
+//Sobre
+revelar.reveal('.scroll-animation1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '120px',
+    reset: false
+})
+revelar.reveal('.scroll-animation2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '120px',
+    reset: false
+})
+
+//Convidados
+revelar.reveal('.card-animation1', {
+    duration: 1000,
+    origin: 'right',
+    distance: '120px',
+    delay: 200,
+    reset: false
+})
+revelar.reveal('.card-animation2', {
+    duration: 1000,
+    origin: 'right',
+    distance: '120px',
+    delay: 400,
+    reset: false
+})
+revelar.reveal('.card-animation3', {
+    duration: 1000,
+    origin: 'right',
+    distance: '120px',
+    delay: 600,
+    reset: false
+})
+revelar.reveal('.card-animation4', {
+    duration: 1000,
+    origin: 'right',
+    distance: '120px',
+    delay: 800,
+    reset: false
+})
+
+revelar.reveal('.card-animation5', {
+    duration: 1000,
+    origin: 'left',
+    distance: '120px',
+    delay: 200,
+    reset: false
+})
+revelar.reveal('.card-animation6', {
+    duration: 1000,
+    origin: 'left',
+    distance: '120px',
+    delay: 400,
+    reset: false
+})
+revelar.reveal('.card-animation7', {
+    duration: 1000,
+    origin: 'left',
+    distance: '120px',
+    delay: 600,
+    reset: false
+})
+revelar.reveal('.card-animation8', {
+    duration: 1000,
+    origin: 'left',
+    distance: '120px',
+    delay: 800,
+    reset: false
+})
